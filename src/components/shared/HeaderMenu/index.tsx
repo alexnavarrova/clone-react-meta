c30a0f66ef83e9bbdba1be06432cb44c48138a2f
Alexander Navarro <alexandernavarro@Alexanders-MacBook-Pro.local>
2024-04-30 12:39:35 -0500

    Se agrego estructura menu

---
+++ src/components/shared/HeaderMenu/index.tsx
@@ -0,0 +1,148 @@
+export const HeaderMenu = () => (
+  <>
+    {/* Menu >> Meta Quest */}
+    <div className="py-16 border-y bg-gray-200">
+      <div className="max-w-5xl mx-auto flex justify-end gap-16 text-xl">
+        <div className="w-60">
+          <ul className="space-y-4">
+            <li className="font-bold text-base text-gray-500">
+              Informacion General sobre Meta Quest
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+                Meta Quest 3
+              </span>
+              <span className="py-1 px-2 bg-[#f5ebfa] font-medium text-xs text-[#a121ce] rounded-sm">
+                Novedad
+              </span>
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+                Meta Quest 2
+              </span>
+              <span className="py-1 px-2 bg-[#f5ebfa] font-medium text-xs text-[#a121ce] rounded-sm">
+                Oferta especial
+              </span>
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Meta Quest Pro
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Compara dispositivos
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Accesorios
+            </li>
+          </ul>
+        </div>
+        <div className="w-60">
+          <ul className="space-y-4">
+            <li className="font-bold text-base text-gray-500">
+              Aplicaciones y juegos
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Meta Quest+
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Experiencias de VR
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    {/* End Menu >> Meta Quest */}
+
+    {/* Menu >> Ray-Ban | Meta */}
+    <div className="py-16 border-y bg-gray-200">
+      <div className="max-w-5xl mx-auto text-xl">
+        <div className="w-60">
+          <ul className="space-y-4">
+            <li className="font-bold text-base text-gray-500">
+              Informacion General sobre Ray-Ban | Meta
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Wayfarer
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Headliner
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Explora todo
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    {/* End Menu >> Ray-Ban | Meta */}
+
+    {/* Menu >> Asistencia */}
+    <div className="py-16 border-y bg-gray-200">
+      <div className="max-w-5xl mx-auto flex gap-16 text-xl">
+        <div className="w-60">
+          <ul className="space-y-4">
+            <li className="font-bold text-base text-gray-500">
+              Servicios de ayuda de la tienda y los dispositivos
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Encuentra tu pedido
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Meta Quest y Quest Pro
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Meta Portal
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Gafas Inteligentes
+            </li>
+          </ul>
+        </div>
+        <div className="w-60">
+          <ul className="space-y-4">
+            <li className="font-bold text-base text-gray-500">
+              Servicios de ayuda de Meta
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Centro de cuentas
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Meta Pay
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Politicas
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Experiencias conectadas
+            </li>
+          </ul>
+        </div>
+        <div className="w-60">
+          <ul className="space-y-4">
+            <li className="font-bold text-base text-gray-500">
+              Servicios de ayuda de las aplicaciones
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Meta Horizon
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Facebook
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Messenger
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Instagram
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              WhatsApp
+            </li>
+            <li className="hover:underline hover:underline-offset-2 hover:decoration-1 hover:cursor-pointer">
+              Workplace
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    {/* End Menu >> Asistencia */}
+  </>
+);
--- src/layouts/LandingLayout.tsx
+++ src/layouts/LandingLayout.tsx
@@ -1,10 +1,11 @@
+import { HeaderMenu } from "../components/shared/HeaderMenu";
 import { Header } from "../components/shared/header";
 import { TopBar } from "../components/shared/top-bar";
 
-
 export const LandingLayout = () => (
-    <>
-        <TopBar />
-        <Header />
-    </>
+  <>
+    <TopBar />
+    <Header />
+    <HeaderMenu />
+  </>
 );
